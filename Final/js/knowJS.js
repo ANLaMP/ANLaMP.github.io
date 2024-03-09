bcc19c3f1b52618f6733fa5b22f0372c9ffe0823
pattersonanthonyl <144876518+ANLaMP@users.noreply.github.com>
2024-03-09 00:03:55 -0700

    Add files via upload

---
+++ Final/js/knowJS.js
@@ -0,0 +1,19 @@
+/*********************************************************************
+***
+*Original Author:    Anthony Patterson           *
+*Date Created:       03/06/2024                  *
+*Version:            1                           *
+*Date Last Modified: 03/06/2024                  *
+*Modified by:        Anthony Patterson           *
+*Modification log:                               *
+***
+******************************************************************** */
+"use strict";
+
+$(document).ready(function(){
+    $(".carousel").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+});
